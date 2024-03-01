17f0b212d6455aeba8a53018ac45dfa7abafd30a
Matt Zahar <matthewzahar@gmail.com>
2024-03-01 10:21:41 -0500

    final cleanup

--- dags/cloudquery_dag.py
+++ dags/cloudquery_dag.py
@@ -4,15 +4,16 @@ import subprocess
 import os
 
 from airflow.datasets import Dataset
-
 from airflow.decorators import dag, task
+
 @dag(
-    schedule_interval='@daily',
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    schedule_interval='@daily', # run it daily
+    start_date=pendulum.datetime(2024, 2, 1, tz="America/New_York"),
     catchup=False,
     tags=["example"],
 )
 def run_cloudquery():
+    # single task to run the cloudquery cli in airflow
     @task(outlets=[Dataset("source.ally.btc.coinpaprika_tickers")])
     def call_sync(): 
         commands = [f"{os.environ['AIRFLOW_HOME']}/./cloudquery", "sync", "conf.yml", "postgres.yml"]
--- dags/my_cosmos_dag.py
+++ dags/my_cosmos_dag.py
@@ -4,13 +4,14 @@ import os
 from airflow.datasets import Dataset
 import pendulum
 
+# use local profile
 profile_config = ProfileConfig(
     profile_name="ally",
     target_name="dev",
     profiles_yml_filepath=f"{os.environ['AIRFLOW_HOME']}/dags/dbt/profiles.yml",
 )
 
-
+# single dag with several task groups for each model
 my_cosmos_dag = DbtDag(
     project_config=ProjectConfig(
         "/usr/local/airflow/dags/dbt",
@@ -21,7 +22,7 @@ my_cosmos_dag = DbtDag(
     ),
     # normal dag parameters
     schedule=[Dataset("source.ally.btc.coinpaprika_tickers")],
-    start_date=pendulum.datetime(2023, 1, 1, tz="America/New_York"),
+    start_date=pendulum.datetime(2023, 2, 1, tz="America/New_York"),
     catchup=False,
     dag_id="my_cosmos_dag",
     default_args={"retries": 2},
